6374fef49b8490f896e39a552cbafa2e36c4f1d1
toaster545 <mlabrie0208@gmail.com>
2026-04-04 18:49:22 -0400

    Graphs tp2

--- src/solvers/re_optimizer.py
+++ src/solvers/re_optimizer.py
@@ -136,26 +136,17 @@ class ReOptimizer(Solver):
             offline_model : OfflineSolver instance (Gurobi MIP model).
 
         """
-        delta = 120
-
+        WINDOW = 120  # ±2 minutes in seconds
         U_prev = self.initial_solution['U']
-        Z_prev = self.initial_solution['Z']
-
         for f_i in P:
-            if f_i.id in Z_prev and Z_prev[f_i.id] > 0.5:
+            if f_i.id in U_prev:
                 u_val = U_prev[f_i.id]
-                lb = max(f_i.ready_time, u_val - delta)
-                ub = min(f_i.latest_pickup, u_val + delta)
-                offline_model.model.addConstr(
-                    offline_model.U_var[f_i.id] >= lb,
-                    name=f"fix_arrival_lb_{f_i.id}"
-                )
-                offline_model.model.addConstr(
-                    offline_model.U_var[f_i.id] <= ub,
-                    name=f"fix_arrival_ub_{f_i.id}"
-                )
-
+                lb = max(f_i.ready_time, u_val - WINDOW)
+                ub = min(f_i.latest_pickup, u_val + WINDOW)
+                offline_model.U_var[f_i.id].LB = lb
+                offline_model.U_var[f_i.id].UB = ub
         offline_model.model.update()
+
 
     def destroy_fix_variables(self, K, P, offline_model: OfflineSolver):
         """ Fix some of Y_var, X_var variables based on the initial solution.
@@ -171,40 +162,29 @@ class ReOptimizer(Solver):
             - Forbid the arcs that goes from departing node of a vehicle to other requests that were in different
                   vehicle
         """
-        # Build a mapping: trip_id -> vehicle_id from the previous solution
+        # Build trip -> vehicle mapping from previous solution
         trip_vehicle = {}
         for vehicle_id, data in self.initial_solution['assignment_dict'].items():
             for trip in data['assigned_requests']:
                 trip_vehicle[trip.id] = vehicle_id
 
-        X_prev = self.initial_solution['X']
-        Y_prev = self.initial_solution['Y']
+        # Forbid Y[k, i] when trip i was previously served by a different vehicle
+        for f_k in K:
+            for f_i in P:
+                if f_i.id in trip_vehicle and trip_vehicle[f_i.id] != f_k.id:
+                    offline_model.Y_var[f_k.id, f_i.id].UB = 0
 
+        # Forbid X[i, j] when trips i and j were previously in different vehicles
         for f_i in P:
             for f_j in P:
                 if f_i != f_j:
-                    veh_i = trip_vehicle.get(f_i.id)
-                    veh_j = trip_vehicle.get(f_j.id)
-                    # Only forbid arcs between two served trips from different vehicles.
-                    # Leave arcs involving unserved trips free so the solver can assign them.
-                    if veh_i is not None and veh_j is not None and veh_i != veh_j:
+                    v_i = trip_vehicle.get(f_i.id)
+                    v_j = trip_vehicle.get(f_j.id)
+                    if v_i is not None and v_j is not None and v_i != v_j:
                         offline_model.X_var[f_i.id, f_j.id].UB = 0
-                    # Positively fix arcs that were active in the previous solution
-                    elif veh_i is not None and veh_i == veh_j:
-                        if X_prev.get(f_i.id, {}).get(f_j.id, 0) > 0.5:
-                            offline_model.X_var[f_i.id, f_j.id].LB = 1
-
-        for f_k in K:
-            for f_i in P:
-                veh_i = trip_vehicle.get(f_i.id)
-                if veh_i is not None and veh_i != f_k.id:
-                    offline_model.Y_var[f_k.id, f_i.id].UB = 0
-                # Positively fix Y if this was the first trip of vehicle k
-                elif veh_i is not None and veh_i == f_k.id:
-                    if Y_prev.get(f_k.id, {}).get(f_i.id, 0) > 0.5:
-                        offline_model.Y_var[f_k.id, f_i.id].LB = 1
 
         offline_model.model.update()
+
 
     def destroy_bonus(self, K, P, offline_model: OfflineSolver):
         """Custom destroy method: fix well-served trips, free poorly-served ones.
